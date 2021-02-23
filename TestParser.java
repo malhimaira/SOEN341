8cf254e8c75b02e4c78dedd5fa30ab0ed078dd5b
btakli <64867481+btakli@users.noreply.github.com>
2021-02-22 21:39:55 -0500

    Fixed TestParser Expected Output

--- TestParser.java
+++ TestParser.java
@@ -32,7 +32,7 @@ public class TestParser {
 
 
         System.out.println("Test Parser");
-        System.out.println("[halt 1, and D, shl 18, tgt 1D, exit 3]");
+        System.out.println("[halt 0, and D, shl 18, tgt 1D, exit 3]");
 
 
         parzival.parseTokens(dlex);
