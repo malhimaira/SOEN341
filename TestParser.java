a41ba6338a7a3bc7fcf3cabf4e19220d6db2c5af
btakli <64867481+btakli@users.noreply.github.com>
2021-02-22 21:22:49 -0500

    Tested TestParser

--- TestParser.java
+++ TestParser.java
@@ -31,8 +31,8 @@ public class TestParser {
         DummyLexer dlex = new DummyLexer(array);
 
 
-        System.out.println("TestParser");
-        System.out.println("[halt 1, and D, shl 18, tgt 1D, exit 3]");
+        System.out.println("Test Parser");
+        System.out.println("[halt 0, and D, shl 18, tgt 1D, exit 3]");
 
         parzival.parseTokens(dlex);
         //System.out.println("Actual Output: ");
