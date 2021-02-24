4235d919f630839465d6629d5bd13701527eb34b
Maira Malhi <60297735+malhimaira@users.noreply.github.com>
2021-02-24 11:34:20 -0500

    Fixed Lexer and test FIle

--- Lexer.java
+++ Lexer.java
@@ -1,11 +1,9 @@
-import java.io.EOFException;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.ArrayList;
 
-import java.util.Iterator;
-import java.util.Map;
+
 
 /**
  * Lexer Class used to create Token Objects from a given fileStream to send to the Parser
@@ -13,7 +11,6 @@ import java.util.Map;
 public class Lexer implements ILexer{
 
 	private HashMap<String, Token> SymbolTable = new HashMap<String, Token>();
-	private Iterator<Map.Entry<String, Token>> iter;
 	private ArrayList<String> TokenSequence;
 	private int cntString;
 
@@ -41,7 +38,7 @@ public class Lexer implements ILexer{
 						Mnemonic m1 = new Mnemonic(word.trim()); //needs to be more generalized more after //TODO Brandon added trim
 					SymbolTable.put(m1.getName(), m1);
 					TokenSequence.add(m1.getName()); //TODO Maira
-					System.out.println(m1); //TODO Brandon
+					//System.out.println(m1); //TODO Brandon
 					word="";
 					firstIter = true; //TODO Brandon
 					}
@@ -49,21 +46,21 @@ public class Lexer implements ILexer{
 					EOL e1 = new EOL("EOL");
 					SymbolTable.put(e1.getName(), e1);
 					TokenSequence.add(e1.getName()); //TODO Maira
-					System.out.println(e1); //TODO Brandon
+					//System.out.println(e1); //TODO Brandon
 					continue;
 				}
 				if ((char)metaChar == ' ' && !firstIter && !word.trim().equals("")) { //just to account the space at the beginning, space char included for now as thats what is in the asm
 					Mnemonic m1 = new Mnemonic(word.trim()); //needs to be more generalized more after //TODO Brandon added trim 
 					SymbolTable.put(m1.getName(), m1);
 					TokenSequence.add(m1.getName()); //TODO Maira
-					System.out.println(m1); //TODO Brandon
+					//System.out.println(m1); //TODO Brandon
 					word="";
 					firstIter = true; //TODO Brandon
 				}
 				else {
 					firstIter = false;
 					word += (char)metaChar;
-					System.out.println(word);
+					//System.out.println(word);
 				}
 			}
 			//CHECK FOR LAST ITERATION IN CASE 
@@ -77,8 +74,8 @@ public class Lexer implements ILexer{
 			SymbolTable.put(eof.getName(), eof);
 			TokenSequence.add(eof.getName());  //TODO Maira
 			//System.out.println("<eof>");
-			iter = SymbolTable.entrySet().iterator();
-			System.out.println(SymbolTable);
+			
+			
 		}
 		catch(IOException e) {
 			System.out.println(e.getMessage());
@@ -86,61 +83,22 @@ public class Lexer implements ILexer{
 	}
 	
 	/**
-	 * Trial to have a parameterized get token method- case where parser asks for a type of token
+	 * Get The Symbol Table (in case of parameter
+	 * @return
 	 */
-	public Token getNextToken(String tokentype) {
-		
-		//Stack and request all types in Parser and and give each possible key *** NEED POSITION!!!
-		//will remain unordered and the position would need to handle this.
-		if(SymbolTable.containsKey(tokentype)) {
-			return SymbolTable.get(tokentype);
-		}
-		
-		// ask for a token type (mnemonics, EOL or EOF)
-		//Search the  HashMap for the next one of thus and keep track?
-		if(SymbolTable.containsKey(tokentype)) {
-			
-		}
-		return null;
+	public HashMap<String, Token> getSymbolTable() {
+		return SymbolTable;
 	}
 	
 	
-	
 	/**
-	 * OLD
+	 * Method for returning tokens sequentially
 	 * @return Token
 	 */
 	public Token getNextToken() {
 		Token t = null;
-		if(iter.hasNext()) {
-						HashMap.Entry<String, Token> me = (HashMap.Entry<String, Token>) iter.next();
-			if(me.getValue().getClass() == Token.class ) {
-				t = (Token) me.getValue();
-				return t;
-			}
-			else if(me.getValue().getClass() == Mnemonic.class) {
-				t = (Mnemonic) me.getValue();
-				return t;
-			}
-			else if(me.getValue().getClass() == EOL.class) {
-				t = (EOL) me.getValue();
-				return t;
-			}
-			else if(me.getValue().getClass() == EOF.class) {
-				t = (EOF) me.getValue();
-				return t;
-			}
-			return t; 
-		}
-		else {
-			return t; //Team 4 please handle the case where the Token is null, if it is then just don't process it.
-		}	
-	}
-	
-	public Token getNextTokenArray() {
-		Token t = null;
 		
-		if (SymbolTable.get(TokenSequence.get(cntString))!= null) {
+		if (cntString != TokenSequence.size() && SymbolTable.get(TokenSequence.get(cntString))!= null) {
 			t = SymbolTable.get(TokenSequence.get(cntString));
 			cntString++;
 			return t; 
@@ -149,5 +107,6 @@ public class Lexer implements ILexer{
 			return t; //Team 4 please handle the case where the Token is null, if it is then just don't process it.
 		}	
 	}
+	
 
 }
--- testMain.java
+++ testMain.java
@@ -3,9 +3,15 @@ import java.io.FileInputStream;
 public class testMain {
     public static void main(String args[]) {
         try {
-            FileInputStream reader = new FileInputStream("C:\\Users\\chanj\\Downloads\\TestInherentMnemonics.asm");
+            FileInputStream reader = new FileInputStream("TestInherentMnemonics.asm");
             Lexer lex =  new Lexer(reader);
-            System.out.println(lex.getNextToken());
+            
+            Token t = lex.getNextToken();
+            while(t != null) {
+            	System.out.println(t);
+            	t = lex.getNextToken();
+            }
+           
         }
         catch(Exception e){
             System.out.println(e.getMessage());
