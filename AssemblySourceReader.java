e8400cf9e1dbe33f1c10507c0f9a52e52d19caeb
btakli <64867481+btakli@users.noreply.github.com>
2021-02-24 14:54:38 -0500

    Removed Useless Imports

--- AssemblySourceReader.java
+++ AssemblySourceReader.java
@@ -1,15 +1,8 @@
-package com.example.soen341;
-
-import java.awt.Desktop;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 public class AssemblySourceReader {
 
