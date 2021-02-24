7085b8201dc85bbb6ea4f1f7bf56cc4ba717a77f
AmelieSing <AmelieSing@users.noreply.github.com>
2021-02-24 14:43:45 -0500

    updating

--- TestGenerateList.java
+++ TestGenerateList.java
@@ -9,42 +9,17 @@ public class TestGenerateList extends Exception{
       System.out.println("Testing Generated List classes:");
 
       /*Parts from TestParse as we need to test if the ListingGenerator can actually do it's job*/
-      // Initializing a Parser object to run the test
-      Parser parserObject = new Parser();
-
-      // Initializing an ArrayList of Tokens to use as a Hardcoded Test case
-      ArrayList<Token> tokenList = new ArrayList<Token>();
-
-      //Adding Tokens to the ArrayList
-      tokenList.add(new Mnemonic("halt"));
-      tokenList.add(new EOL("EOL"));
-
-      tokenList.add(new Mnemonic("and"));
-      tokenList.add(new EOL("EOL"));
-
-      tokenList.add(new Mnemonic("shl"));
-      tokenList.add(new EOL("EOL"));
-
-      tokenList.add(new Mnemonic("tgt"));
-      tokenList.add(new EOL("EOL"));
-
-      tokenList.add(new Mnemonic("exit"));
-      tokenList.add(new EOL("EOL"));
-
-      tokenList.add(new EOF("EOF"));
-
-      //Instantiating a DummyLexer Object with filled TokenList
-      DummyLexer dlex = new DummyLexer(tokenList);
+      //Instantiating a DummyLexer Object with filled, hard-coded list of tokens.
+      ILexer dlex = new DummyLexer();
+      //Instantiating a parser using the dummy lexer.
+      IParser parserObject = new Parser(dlex);
 
       //Printing the expected output
-      //System.out.println("Test Parser");
-      //System.out.println("[halt 0, and D, shl 18, tgt 1D, exit 3]");
-
-      //Calling the Parser on the DummyLexer Object
-      parserObject.parseTokens(dlex);
+      System.out.println("Test Parser");
+      System.out.println("[halt 0, and D, shl 18, tgt 1D, exit 3]");
 
       //Printing the actual output
-     // System.out.println(parserObject.getIR());
+      System.out.println(parserObject.parse());
       /*End of stuff taken for testing purposes from TestParser*/
 
       //Implementation of what is within the stub and ListingGenerator file
@@ -53,11 +28,11 @@ public class TestGenerateList extends Exception{
 
       ListingGenerator testListing = new ListingGenerator(parserObject.getIR());
 
-      System.out.printf("%-30.30s", ListingGenerator.header+"\n");
+      System.out.println(ListingGenerator.header + "\n");
       int i = 1;
       for(LineStatement ls : testListing.getStub().getArrayLineStat()){
         //setAddr(i);
-        System.out.printf(i + "\t" + ls.toString()+"\n");
+        System.out.println(i + "\t" + ls.toString()+"\n");
         i++;
       }
     }
