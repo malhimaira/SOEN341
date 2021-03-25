63cd91488e3c2e870d17a156f9e99e8a0ab65521
btakli <64867481+btakli@users.noreply.github.com>
2021-03-25 16:50:38 -0400

    Removed commented out code

--- TestErrorReporter.java
+++ TestErrorReporter.java
@@ -6,13 +6,6 @@ public class TestErrorReporter{
             errorReporter.record(new ErrorMsg("Error! This should be also reported!", new Position(2,0)));  
             errorReporter.record(new ErrorMsg("Error! This should be also reported too!", new Position(3,0)));
             System.out.println("Test ErrorReporter");
-            // System.out.println("File.asm:line 1, column 0: Error! This should be reported!");
-            // System.out.println("File.asm:line 2, column 0: Error! This should be also reported!");
-            // System.out.println("File.asm:line 3, column 0: Error! This should be also reported too!");
-            // System.out.println();
-            // System.out.println();
-            // System.out.println();
-            // System.out.println("3 errors found.");
 
             errorReporter.report();
 
