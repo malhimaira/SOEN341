8c6a5b1b341fa2f436f810c975581f1e095bb3b1
btakli <64867481+btakli@users.noreply.github.com>
2021-04-17 16:07:27 -0400

    Added Automated Test for EXE dump using his Dump class

--- AutomatedTestRunner.bat
+++ AutomatedTestRunner.bat
@@ -20,5 +20,12 @@ echo "Running ErrorReporter related tests (for ErrorReporter itself, Parser and 
 java TestErrorReporter.java > ErrorReporterTests.txt
 java TestParserErrors.java >> ErrorReporterTests.txt
 java TestErrorReporterLexer >> ErrorReporterTests.txt
-echo "Done!"
+
+echo "Testing Dump Output of CMA EXE generation for rela01.exe (output to TestDump.txt)"
+java CMA rela01.asm
+(echo Test Dump Rela01) > TestDump.txt
+(echo 0000: d5 00 08 d9 0c d5 00 06 41 31 00 42 32 33 00       ........A1.B23. ) >> TestDump.txt
+java Dump rela01.exe 0 >> TestDump.txt
+java aunit TestDump.txt
+echo "Tests Complete!"
 pause
